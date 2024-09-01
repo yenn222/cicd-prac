111ba9f7ec96264dd7b99f8f536e585f896e7bf5
yenn222 <20221038@o365.hanbat.ac.kr>
2024-09-01 15:34:50 +0900

    파일명

--- main/app.ts
+++ main/app.ts
@@ -1,14 +1,12 @@
+
+
 const server = Bun.serve({
   port: 8000,
   async fetch(req) {
     const path = new URL(req.url).pathname;
 
     if (path === "/") {
-      return new Response("./yenn.html",{
-        headers: {
-          "Content-Type": "text/html; charset=utf-8"
-        }
-      });
+      return new Response(Bun.file("./yenn.html"));
     }
 
     return new Response("Not found", { status: 404 });
